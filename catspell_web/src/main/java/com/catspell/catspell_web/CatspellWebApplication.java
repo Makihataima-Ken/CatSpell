8d7843cd92444d944b15170302c7ff47220c885a
Ken <ahmadsoud63@gmail.com>
2025-04-14 17:20:43 +0300

    intial spring boot project

---
+++ catspell_web/src/main/java/com/catspell/catspell_web/CatspellWebApplication.java
@@ -0,0 +1,13 @@
+package com.catspell.catspell_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatspellWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatspellWebApplication.class, args);
+	}
+
+}
---
+++ catspell_web/src/test/java/com/catspell/catspell_web/CatspellWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catspell.catspell_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatspellWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
